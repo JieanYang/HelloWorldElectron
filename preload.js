2d8ff134c71a0dceeae08d62c367b7c637294fbd
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-05 17:21:23 +0200

    使用 preload.js 将 Electron 的 processprocess.versions 对象暴露给渲染器

--- preload.js
+++ preload.js
@@ -1,3 +1,12 @@
+const { contextBridge } = require('electron');
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // 能暴露的不仅仅是函数，我们还可以暴露变量
+});
+
 // 预加载脚本有权访问两个 渲染器全局 (例如 `window` 和 `document`) 和 Node.js 环境 `process`。
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
--- renderer.js
+++ renderer.js
@@ -1,1 +1,4 @@
 console.log('run renderer.js');
+
+const information = document.getElementById('info');
+information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 和 Electron (v${versions.electron()})`;
