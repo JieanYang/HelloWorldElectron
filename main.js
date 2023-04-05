7393c9b4a6e575e1da0ef72c245cd97c2b878809
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-05 16:29:26 +0200

    增加对应 Linux, Windows, MacOs 系统的窗口控件 代码

--- main.js
+++ main.js
@@ -13,4 +13,15 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+
+  // macOS 应用通常即使在没有打开任何窗口的情况下也继续运行，并且在没有窗口可用的情况下激活应用时会打开新的窗口
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  // 在Windows和Linux上，关闭所有窗口通常会完全退出一个应用程序
+  // macOS 应用通常即使在没有打开任何窗口的情况下也继续运行，并且在没有窗口可用的情况下激活应用时会打开新的窗口
+  if (process.platform !== 'darwin') app.quit();
 });
