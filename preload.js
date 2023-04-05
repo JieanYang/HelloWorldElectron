87371d8928bac5864512f92cd7e7f71bc2066a1d
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-05 17:57:06 +0200

    Setup index UI to trigger method ipcRenderer.send in preload.js

--- preload.js
+++ preload.js
@@ -11,6 +11,10 @@ contextBridge.exposeInMainWorld('versions', {
   // 能暴露的不仅仅是函数，我们还可以暴露变量
 });
 
+contextBridge.exposeInMainWorld('electronAPI', {
+  setTitle: (title) => ipcRenderer.send('set-title', title),
+});
+
 // 预加载脚本有权访问两个 渲染器全局 (例如 `window` 和 `document`) 和 Node.js 环境 `process`。
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
