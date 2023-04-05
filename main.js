82287cd8e9e9a7159a111dcd315b00aebe78f90a
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-04-05 23:25:50 +0200

    Disable load web

--- main.js
+++ main.js
@@ -86,13 +86,15 @@ app.whenReady().then(() => {
   // Menu.setApplicationMenu(menu);
   // ================== The menu component bans the development tool for web - end ==================
 
-  mainWindow.loadURL('https://google.com');
+  // ================== Load web - start ==================
+  // mainWindow.loadURL('https://google.com');
 
-  mainWindow.webContents.on('did-finish-load', () => {
-    const scriptPath = path.join(__dirname, 'renderer_2.js');
-    const scriptContents = fs.readFileSync(scriptPath, 'utf8');
-    mainWindow.webContents.executeJavaScript(scriptContents);
-  });
+  // mainWindow.webContents.on('did-finish-load', () => {
+  //   const scriptPath = path.join(__dirname, 'renderer_2.js');
+  //   const scriptContents = fs.readFileSync(scriptPath, 'utf8');
+  //   mainWindow.webContents.executeJavaScript(scriptContents);
+  // });
+  // ================== Load web - end ==================
 
   // macOS 应用通常即使在没有打开任何窗口的情况下也继续运行，并且在没有窗口可用的情况下激活应用时会打开新的窗口
   app.on('activate', () => {
